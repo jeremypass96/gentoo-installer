a9ac059990eb9451bdfa79be1a502abc46682ded
Jeremy Passarelli <recordguy96@aol.com>
2026-02-18 10:34:44 -0500

    Fixed bug.

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
+SCRIPT_DIR="$(cd -- "$(dirname -- "$1")" && pwd)"
 source "${SCRIPT_DIR}/modules/common.sh"
 require_root
 require_not_chroot
