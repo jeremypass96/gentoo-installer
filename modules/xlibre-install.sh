6d8c768720d8ce1395f236e43805463100ac39f1
Jeremy Passarelli <recordguy96@aol.com>
2026-02-19 21:37:16 -0500

    Fix XLibre installer.

--- modules/xlibre-install.sh
+++ modules/xlibre-install.sh
@@ -28,15 +28,9 @@ emaint sync -r xlibre
 # Install Xlibre.
 echo "*/*::xlibre ~amd64" > /etc/portage/package.accept_keywords/xlibre
 chmod go+r /etc/portage/package.accept_keywords/xlibre
-emerge -f --autounmask=y --autounmask-write x11-base/xlibre-server
-etc-update --automode -3
+emerge -f x11-base/xlibre-server
 emerge -C x11-base/xorg-server
 emerge -C x11-base/xorg-drivers
-emerge x11-base/xlibre-server
+emerge -av1 x11-base/xlibre-server
 emerge @x11-module-rebuild
 emerge @preserved-rebuild
-
-# Disable X11 support for SDDM. Pulls in x11-base/xorg-server.
-echo "x11-misc/sddm -X" > /etc/portage/package.use/sddm
-chmod go+r /etc/portage/package.use/sddm
-emerge -uU x11-misc/sddm
