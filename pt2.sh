47cc06bdc6db0a2f320d6419582fc31f60655fb6
Jeremy Passarelli <recordguy96@aol.com>
2026-06-05 18:19:28 -0400

    Changed default umask to 022.

--- pt2.sh
+++ pt2.sh
@@ -807,7 +807,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
 cat <<EOF >>/etc/skel/.zshrc
 # Set the default umask.
-umask 077
+umask 022
 
 # Disable highlighting of pasted text.
 zle_highlight=('paste:none')
