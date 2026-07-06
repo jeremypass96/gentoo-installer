3a3867cf2bfc84ce479f9743260e6d6bc6e4d94b
Jeremy Passarelli <recordguy96@aol.com>
2026-07-06 16:57:58 -0400

    Filter out systemd and (exp) profiles.

--- modules/profile-selector.sh
+++ modules/profile-selector.sh
@@ -56,6 +56,8 @@ while IFS= read -r line; do
 		idx="${BASH_REMATCH[1]}"
 		desc="${BASH_REMATCH[2]}"
 		[[ "$desc" != *"/desktop"* ]] && continue
+		[[ "$desc" == *"/systemd"* ]] && continue
+		[[ "$desc" == *"(exp)"* ]] && continue
 		PROFILE_MAP["$idx"]="$desc"
 		PROFILE_MENU+=("$idx" "$desc")
 	fi
